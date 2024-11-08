c1a34aa78ebb07b6a6e5b3984e85299dcea60398
Salim BENABDELLAH <salim.benabdellah@hotmail.fr>
2024-11-08 20:18:49 +0100

    Added unit tests for the CheckProductModelErrors method in ProductService

--- P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
+++ P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
@@ -1,26 +1,62 @@
 ﻿using Xunit;
+using Moq;
+using System.Collections.Generic;
+using Microsoft.Extensions.Localization;
+using P3AddNewFunctionalityDotNetCore.Application.Services;
+using P3AddNewFunctionalityDotNetCore.Data.Models.ViewModels;
+using P3AddNewFunctionalityDotNetCore.Infrastructure.Repositories;
+using NSubstitute;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 
 namespace P3AddNewFunctionalityDotNetCore.Tests
 {
-    public class ProductServiceTests
+
+
+    public class ProductServiceTeste
     {
-        /// <summary>
-        /// Take this test method as a template to write your test method.
-        /// A test method must check if a definite method does its job:
-        /// returns an expected value from a particular set of parameters
-        /// </summary>
-        [Fact]
-        public void ExampleMethod()
+        public readonly ProductService _sut;
+        public readonly IProductRepository _productRepository = Substitute.For<IProductRepository>();
+        public readonly ICartService _cartService = Substitute.For<ICartService>();
+        public readonly IOrderRepository _orderRepository = Substitute.For<IOrderRepository>();
+        public readonly IStringLocalizer<ProductService> _stringLocalizer = Substitute.For<IStringLocalizer<ProductService>>();
+
+        public ProductServiceTeste()
         {
-            // Arrange
-
-            // Act
-
-
-            // Assert
-            Assert.Equal(1, 1);
+            _sut = new(_cartService, _productRepository, _orderRepository, _stringLocalizer);
         }
 
-        // TODO write test methods to ensure a correct coverage of all possibilities
+        [Theory]
+        [InlineData(null, "10", "5", "MissingName")]
+        [InlineData("Product A", null, "5", "MissingPrice")]
+        [InlineData("Product A", "10", null, "MissingQuantity")]
+        [InlineData("Product A", "a", "5", "PriceNotANumber")]
+        [InlineData("Product A", "0", "5", "PriceNotGreaterThanZero")]
+        [InlineData("Product A", "10", "0", "StockNotGreaterThanZero")]
+        [InlineData("Product A", "10", "b", "StockNotAnInteger")]
+        public void CheckProductModelErrors_ShouldReturnExpectedError(string name, string price, string stock, string expectedError)
+        {
+            // Arrange
+            var product = new ProductViewModel { Name = name, Price = price, Stock = stock };
+            _stringLocalizer[expectedError].Returns(new LocalizedString(expectedError, expectedError));
+
+            // Act
+            var errors = _sut.CheckProductModelErrors(product);
+
+            // Assert
+            Assert.Contains(expectedError, errors);
+        }
+
+        [Fact]
+        public void CheckProductModelErrors_ShouldReturnEmptyList_WhenValidProduct()
+        {
+            // Arrange
+            var product = new ProductViewModel { Name = "Valid Product", Price = "100", Stock = "10" };
+
+            // Act
+            var errors = _sut.CheckProductModelErrors(product);
+
+            // Assert
+            Assert.Empty(errors);
+        }
     }
 }
